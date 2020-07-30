00672db3f5f529d431a8585272b7572d1b87d541
gregk27 <git@gregk.ca>
2020-07-30 12:54:59 -0400

    Added checkstyle

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -15,6 +15,8 @@ import edu.wpi.first.wpilibj.RobotBase;
  * call.
  */
 public final class Main {
+
+  /** */
   private Main() {
   }
 
@@ -22,6 +24,7 @@ public final class Main {
    * Main initialization function. Do not perform any initialization here.
    *
    * <p>If you change your main robot class, change the parameter type.
+   * @param args Command line arguments
    */
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,7 +18,6 @@ import edu.wpi.first.wpilibj.XboxController;
  */
 public class RobotContainer {
 
-
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
